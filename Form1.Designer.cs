881faadc9c70887edb0678e825fb7112b40bef75
Anton Malezhik <Anton Malezhik@PIANISTPC>
2025-03-28 23:42:27 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace LauncherPrototype
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GUI_progessbar = new System.Windows.Forms.ProgressBar();
+            this.GUI_progesslabel = new System.Windows.Forms.Label();
+            this.menustrip = new System.Windows.Forms.MenuStrip();
+            this.ProductToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkUpdatesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteFromDeviceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.startappToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GUI_startbtn = new System.Windows.Forms.Button();
+            this.GUI_labelversion = new System.Windows.Forms.Label();
+            this.menustrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GUI_progessbar
+            // 
+            this.GUI_progessbar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GUI_progessbar.Location = new System.Drawing.Point(12, 303);
+            this.GUI_progessbar.Name = "GUI_progessbar";
+            this.GUI_progessbar.Size = new System.Drawing.Size(466, 23);
+            this.GUI_progessbar.TabIndex = 0;
+            // 
+            // GUI_progesslabel
+            // 
+            this.GUI_progesslabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.GUI_progesslabel.AutoSize = true;
+            this.GUI_progesslabel.Location = new System.Drawing.Point(13, 284);
+            this.GUI_progesslabel.Name = "GUI_progesslabel";
+            this.GUI_progesslabel.Size = new System.Drawing.Size(41, 13);
+            this.GUI_progesslabel.TabIndex = 1;
+            this.GUI_progesslabel.Text = "Статус";
+            // 
+            // menustrip
+            // 
+            this.menustrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ProductToolStripMenuItem});
+            this.menustrip.Location = new System.Drawing.Point(0, 0);
+            this.menustrip.Name = "menustrip";
+            this.menustrip.Size = new System.Drawing.Size(628, 24);
+            this.menustrip.TabIndex = 2;
+            this.menustrip.Text = "menuStrip1";
+            // 
+            // ProductToolStripMenuItem
+            // 
+            this.ProductToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkUpdatesToolStripMenuItem,
+            this.deleteFromDeviceToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.startappToolStripMenuItem});
+            this.ProductToolStripMenuItem.Name = "ProductToolStripMenuItem";
+            this.ProductToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.ProductToolStripMenuItem.Text = "Контент";
+            // 
+            // checkUpdatesToolStripMenuItem
+            // 
+            this.checkUpdatesToolStripMenuItem.Name = "checkUpdatesToolStripMenuItem";
+            this.checkUpdatesToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.checkUpdatesToolStripMenuItem.Text = "Проверить обновление";
+            this.checkUpdatesToolStripMenuItem.Click += new System.EventHandler(this.CheckUpdatesToolStripMenuItem_Click);
+            // 
+            // deleteFromDeviceToolStripMenuItem
+            // 
+            this.deleteFromDeviceToolStripMenuItem.Name = "deleteFromDeviceToolStripMenuItem";
+            this.deleteFromDeviceToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.deleteFromDeviceToolStripMenuItem.Text = "Удалить с устройства";
+            this.deleteFromDeviceToolStripMenuItem.Click += new System.EventHandler(this.DeleteFromDeviceToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(201, 6);
+            // 
+            // startappToolStripMenuItem
+            // 
+            this.startappToolStripMenuItem.Enabled = false;
+            this.startappToolStripMenuItem.Name = "startappToolStripMenuItem";
+            this.startappToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.startappToolStripMenuItem.Text = "Запустить";
+            this.startappToolStripMenuItem.Click += new System.EventHandler(this.StartappToolStripMenuItem_Click);
+            // 
+            // GUI_startbtn
+            // 
+            this.GUI_startbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.GUI_startbtn.Enabled = false;
+            this.GUI_startbtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.GUI_startbtn.Location = new System.Drawing.Point(492, 284);
+            this.GUI_startbtn.Name = "GUI_startbtn";
+            this.GUI_startbtn.Size = new System.Drawing.Size(124, 42);
+            this.GUI_startbtn.TabIndex = 3;
+            this.GUI_startbtn.Text = "Запуск";
+            this.GUI_startbtn.UseVisualStyleBackColor = true;
+            this.GUI_startbtn.Click += new System.EventHandler(this.StartappToolStripMenuItem_Click);
+            // 
+            // GUI_labelversion
+            // 
+            this.GUI_labelversion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.GUI_labelversion.AutoSize = true;
+            this.GUI_labelversion.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.GUI_labelversion.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.GUI_labelversion.Location = new System.Drawing.Point(577, 5);
+            this.GUI_labelversion.Name = "GUI_labelversion";
+            this.GUI_labelversion.Size = new System.Drawing.Size(44, 13);
+            this.GUI_labelversion.TabIndex = 4;
+            this.GUI_labelversion.Text = "Версия";
+            this.GUI_labelversion.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 338);
+            this.Controls.Add(this.GUI_labelversion);
+            this.Controls.Add(this.GUI_startbtn);
+            this.Controls.Add(this.GUI_progesslabel);
+            this.Controls.Add(this.GUI_progessbar);
+            this.Controls.Add(this.menustrip);
+            this.MainMenuStrip = this.menustrip;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Прототип лаунчера";
+            this.menustrip.ResumeLayout(false);
+            this.menustrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar GUI_progessbar;
+        private System.Windows.Forms.Label GUI_progesslabel;
+        private System.Windows.Forms.MenuStrip menustrip;
+        private System.Windows.Forms.ToolStripMenuItem ProductToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkUpdatesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteFromDeviceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startappToolStripMenuItem;
+        private System.Windows.Forms.Button GUI_startbtn;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Label GUI_labelversion;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.IO.Compression;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+
+namespace LauncherPrototype
+{
+    public partial class Form1 : Form
+    {
+        // Переменные лаунчера ===================================================
+
+        // Примечание, внутри ZIP архива, не должно быть вложенных папок (Вплане основная папка в папке)
+        // ✔️ Правильно: PRODUCT_NAME.zip/PRODUCT_EXE
+        // ❌ Неправильно: PRODUCT_NAME.zip/PRODUCT_NAME/PRODUCT_EXE
+
+        // URL к папке на сервере, "/" на конце обязательно!
+        public string SERVER_URL = "http://147.185.221.26:60821/Games Updates/";
+
+        // Имя папки которая будет создана при скачивании контента
+        // Оно же Название ZIP архива на сервере
+        // Оно же и название для .txt файла на сервере с версией
+        public string PRODUCT_NAME = "Project Zomboid Steam Launcher Legacy";
+
+        // Путь к исполняемому файлы, используется как: PRODUCT_NAME/PRODUCT_EXE
+        public string PRODUCT_EXE = "Launcher.exe";
+
+        // =======================================================================
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Проверка файлов при старте
+            CheckAndDownloadServerFiles();
+        }
+
+        // Процедура управления состоянием кнопок запуска
+        public void UpdateStartButtonsState(bool isEnable)
+        {
+            // По умолчанию кнопки отключены
+            bool buttonsEnabled = false;
+
+            // Проверяем условия для включения кнопок только если isEnable=true
+            if (isEnable)
+            {
+                bool folderExists = Directory.Exists(PRODUCT_NAME);
+                bool fileExists = folderExists && File.Exists(Path.Combine(PRODUCT_NAME, PRODUCT_EXE));
+
+                buttonsEnabled = folderExists && fileExists;
+            }
+
+            // Устанавливаем состояние для всех связанных кнопок
+            GUI_startbtn.Enabled = buttonsEnabled;
+            startappToolStripMenuItem.Enabled = buttonsEnabled;
+        }
+
+        // Процедура удаления продукта
+        public async void DeleteProduct()
+        {
+
+            // Отключаем кнопки запуска
+            UpdateStartButtonsState(false);
+
+            // Проверяем папки и файлы и удаляем их
+            if (File.Exists("productversion.txt")) File.Delete("productversion.txt");
+            if (File.Exists("tempProductFile.zip")) File.Delete("tempProductFile.zip");
+            if (Directory.Exists(PRODUCT_NAME)) Directory.Delete(PRODUCT_NAME, true);
+
+            // Уведомление для пользователя
+            GUI_progesslabel.Text = "Удаление выполнено";
+
+        }
+
+        // Процедура распаковки продукта
+        public async void UnpackProduct(string serverVersion)
+        {
+
+            // Уведомление для пользователя
+            GUI_progesslabel.Text = "Начало распаковки...";
+
+            // Начало скачивания с отслеживанием прогресса =========
+            using (var archive = ZipFile.OpenRead("tempProductFile.zip"))
+            {
+                long totalBytes = archive.Entries.Sum(e => e.Length);
+                long totalRead = 0;
+
+                foreach (var entry in archive.Entries)
+                {
+                    string fullPath = Path.Combine(PRODUCT_NAME, entry.FullName);
+
+                    // Это папка
+                    if (entry.Length == 0)
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+                        continue;
+                    }
+
+                    Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+
+                    using (var entryStream = entry.Open())
+                    using (var fileStream = File.Create(fullPath))
+                    {
+                        byte[] buffer = new byte[8192];
+                        int bytesRead;
+
+                        while ((bytesRead = await entryStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                        {
+                            await fileStream.WriteAsync(buffer, 0, bytesRead);
+                            totalRead += bytesRead;
+
+                            var progress = (int)((totalRead * 100) / totalBytes);
+                            GUI_progessbar.Value = progress;
+                            GUI_progesslabel.Text = $"Установка: {progress}%, {totalRead / (1024 * 1024)} MB из {totalBytes / (1024 * 1024)} MB";
+                        }
+                    }
+                }
+            }
+
+            // Удаление архива после распаковки
+            File.Delete("tempProductFile.zip");
+
+            // Уведомление для пользователя
+            GUI_progesslabel.Text = "Установка завершена";
+
+            // Включаем кнопки
+            UpdateStartButtonsState(true);
+
+            // Включаем кнопку удаления с устройства
+            deleteFromDeviceToolStripMenuItem.Enabled = Enabled;
+
+            // Включаем кнопку обновления
+            checkUpdatesToolStripMenuItem.Enabled = Enabled;
+
+            // Обновляем версию
+            File.WriteAllText("productversion.txt", serverVersion);
+
+            // Отображения версии продукта
+            GUI_labelversion.Text = serverVersion;
+
+        }
+
+        // Скачивание zip архива из сервера
+        public async void DownloadZIPfromServer(string serverVersion)
+        {
+            var httpClient = new HttpClient();
+            
+            try {
+
+                using (var response = await httpClient.GetAsync($"{SERVER_URL}{PRODUCT_NAME}.zip", HttpCompletionOption.ResponseHeadersRead))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var totalBytes = response.Content.Headers.ContentLength ?? -1L;
+
+                    using (var stream = await response.Content.ReadAsStreamAsync())
+                    using (var fileStream = new FileStream("tempProductFile.zip", FileMode.Create))
+                    {
+                        var buffer = new byte[8192];
+                        var totalRead = 0L;
+                        var bytesRead = 0;
+
+                        while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                        {
+                            await fileStream.WriteAsync(buffer, 0, bytesRead);
+                            totalRead += bytesRead;
+
+                            if (totalBytes > 0)
+                            {
+                                var progress = (int)((totalRead * 100) / totalBytes);
+                                GUI_progessbar.Value = progress;
+                                GUI_progesslabel.Text = $"Скачивание: {progress}%, {totalRead / (1024 * 1024)} MB из {totalBytes / (1024 * 1024)} MB";
+                            }
+                        }
+                    }
+                }
+
+                // Уведомление для пользователя
+                GUI_progesslabel.Text = "Скачивание завершено";
+
+                // Закрываем соединение с сервером
+                httpClient.Dispose();
+
+                // Начинаем распаковку
+                UnpackProduct(serverVersion);
+
+            } catch (Exception ex) {
+
+                // Отображение ошибки для пользователя
+                GUI_progesslabel.Text = $"Ошибка скачивания: {ex.Message}";
+            }
+        }
+
+        // Процедура проверки и скачивания файлов с сервера
+        public async void CheckAndDownloadServerFiles()
+        {
+
+            try {
+
+                // Выключаем кнопки
+                UpdateStartButtonsState(false);
+                
+                // Отключаем кнопку обновления
+                checkUpdatesToolStripMenuItem.Enabled = false;
+
+                // Проверка существования файла версии
+                if (!File.Exists("productversion.txt"))
+                {
+
+                    // Создаем файл, если он не существует
+                    File.Create("productversion.txt").Close();
+
+                }
+
+                // Получаем версию продукта
+                string localVersion = File.ReadAllText("productversion.txt").Trim();
+
+                // Отображения версии продукта
+                GUI_labelversion.Text = localVersion;
+
+                // Запрос на нужную папку на сервере
+                HttpWebResponse response = (HttpWebResponse)WebRequest.Create(SERVER_URL).GetResponse();
+
+                // Уведомление пользователя
+                GUI_progesslabel.Text = "Сервер в сети";
+
+                try {
+
+                    // Получаем версию из сервера
+                    string serverVersion = await new HttpClient().GetStringAsync($"{SERVER_URL}{PRODUCT_NAME}.txt");
+
+                    // Уведомление пользователя
+                    GUI_progesslabel.Text = "Проверка обновлений...";
+
+                    // Сравниваем версии и проверяем папку с файлом
+                    if (localVersion == serverVersion && Directory.Exists(PRODUCT_NAME) && File.Exists($"{PRODUCT_NAME}/{PRODUCT_EXE}")){
+
+                        // Уведомление пользователя
+                        GUI_progesslabel.Text = "У вас последняя версия ✔️";
+
+                        // Включаем кнопки
+                        UpdateStartButtonsState(true);
+
+                        // Включаем кнопку обновления
+                        checkUpdatesToolStripMenuItem.Enabled = Enabled;
+
+                    } else {
+
+                        // Уведомление пользователя
+                        GUI_progesslabel.Text = "Требуется обновление...";
+
+                        // Спрашиваем пользователя
+                        DialogResult result = MessageBox.Show("Доступно новое обновление! Хотите загрузить его прямо сейчас?", "Обновление", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                        // Выбор ответа
+                        switch (result)
+                        {
+
+                            // Если Да
+                            case DialogResult.Yes:
+
+                                // Отключаем кнопку удаления с устройства
+                                deleteFromDeviceToolStripMenuItem.Enabled = false;
+
+                                // Запуск удаления и скачивания обновления
+                                DeleteProduct();
+
+                                // Скачивание ZIP архива из сервера
+                                DownloadZIPfromServer(serverVersion);
+
+                                break;
+
+                            // Если Нет
+                            case DialogResult.No:
+
+                                // Включаем кнопки
+                                UpdateStartButtonsState(true);
+
+                                // Проверяем, есть ли игровые файлы
+                                if (Directory.Exists(PRODUCT_NAME) && File.Exists($"{PRODUCT_NAME}/{PRODUCT_EXE}"))
+                                {
+
+                                    // Уведомление пользователя
+                                    GUI_progesslabel.Text = "Вы отказались от обновления, можно запускать";
+
+                                } else {
+
+                                    // Уведомление пользователя
+                                    GUI_progesslabel.Text = "Вы отказались от обновления, контент не установлен";
+
+                                }
+
+                                // Включаем кнопку обновления
+                                checkUpdatesToolStripMenuItem.Enabled = Enabled;
+
+                                break;
+                        }
+
+                        // Выход из функции
+                        return;
+
+                    }
+
+                } catch (Exception ex) {
+
+                    // Уведомление пользователя
+                    GUI_progesslabel.Text = $"Ошибка чтения версии: {ex.Message}";
+
+                    // Включаем кнопки
+                    UpdateStartButtonsState(true);
+
+                    // Выход из функции
+                    return;
+
+                }
+
+                // Конец соединения
+                response.Close();
+
+            } catch (WebException ex) when ((ex.Response as HttpWebResponse)?.StatusCode == HttpStatusCode.NotFound) {
+
+                // Уведомление пользователя
+                GUI_progesslabel.Text = "❌ Сервер не отвечает... Попробуйте обновиться позже";
+
+                // Закрываем ответ в случае ошибки
+                ex.Response.Close();
+
+                // Включаем кнопки
+                UpdateStartButtonsState(true);
+            }
+        }
+
+        // Нажатие на кнопку удалить контент
+        private void DeleteFromDeviceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            // Отключаем кнопку обновления
+            checkUpdatesToolStripMenuItem.Enabled = false;
+
+            // Проверка директории на существование
+            if (!Directory.Exists(PRODUCT_NAME)) {
+
+                // Уведомление
+                MessageBox.Show("Программа не нашла контент на удаление", "Удаление с устройства", MessageBoxButtons.OK);
+
+                // Включаем кнопку обновления
+                checkUpdatesToolStripMenuItem.Enabled = Enabled;
+
+            } else {
+
+                // Спрашиваем пользователя
+                DialogResult result = MessageBox.Show("Вы уверены? Вы собираетесь удалить контент с устройства", "Удаление с устройства", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                // Выбор ответа
+                switch (result)
+                {
+
+                    // Если Да
+                    case DialogResult.Yes:
+
+                        // Удаление
+                        DeleteProduct();
+
+                        // Включаем кнопку обновления
+                        checkUpdatesToolStripMenuItem.Enabled = Enabled;
+
+                        break;
+                }
+
+            }
+        }
+
+        // Нажатие на кнопку проверки обновления вручную
+        private void CheckUpdatesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            // Уведомление пользователя
+            GUI_progesslabel.Text = "Запущена проверка обновления...";
+
+            // Начало проверки обновления
+            CheckAndDownloadServerFiles();
+
+        }
+
+        // Действие по запуску продукта
+        private void StartappToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            // Уведомление пользователя
+            GUI_progesslabel.Text = "✔️ Запуск";
+
+            // Выключаем кнопки
+            UpdateStartButtonsState(false);
+
+            // Отключаем кнопку обновления
+            checkUpdatesToolStripMenuItem.Enabled = false;
+
+            try {
+
+                // Запускаем в отдельном процессе
+                string programPath = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}/{PRODUCT_NAME}/{PRODUCT_EXE}";
+
+                // Запуск программы
+                Process.Start(new ProcessStartInfo(programPath) { UseShellExecute = true, WorkingDirectory = Path.GetDirectoryName(programPath) });
+
+                // Закрываем лаунчер
+                Environment.Exit(0);
+
+            } catch {
+
+                // Уведомление пользователя
+                GUI_progesslabel.Text = "❌ Неизвестная ошибка во время запуска!";
+
+                // Включаем кнопку обновления
+                checkUpdatesToolStripMenuItem.Enabled = Enabled;
+
+            }
+
+        }
+    }
+}
